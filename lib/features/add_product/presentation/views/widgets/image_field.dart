2b08f52e9bc32ebb759edc274bf676624c0f9e68
tharwat <tharwatsamy2004@gmail.com>
2024-09-08 17:02:47 +0300

    create add product from part 3

--- lib/features/add_product/presentation/views/widgets/image_field.dart
+++ lib/features/add_product/presentation/views/widgets/image_field.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-class ImageField extends StatelessWidget {
+class ImageField extends StatefulWidget {
   const ImageField({super.key});
 
   @override
+  State<ImageField> createState() => _ImageFieldState();
+}
+
+class _ImageFieldState extends State<ImageField> {
+  bool isLoading = false;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(
-            16,
+    return Skeletonizer(
+      enabled: isLoading,
+      child: GestureDetector(
+        onTap: () async {
+          isLoading = true;
+          setState(() {});
+          try {
+            final ImagePicker picker = ImagePicker();
+
+            final XFile? image =
+                await picker.pickImage(source: ImageSource.gallery);
+          } catch (e) {
+            isLoading = false;
+            setState(() {});
+          }
+          isLoading = false;
+          setState(() {});
+        },
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                16,
+              ),
+              border: Border.all(
+                color: Colors.grey,
+              )),
+          child: const Icon(
+            Icons.image_outlined,
+            size: 180,
           ),
-          border: Border.all(
-            color: Colors.grey,
-          )),
-      child: const Icon(
-        Icons.image_outlined,
-        size: 180,
+        ),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
