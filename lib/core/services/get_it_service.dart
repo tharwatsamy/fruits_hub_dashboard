193b3bb1751ee428d64b2d015297fe09e37a3b8e
tharwat <tharwatsamy2004@gmail.com>
2024-11-06 16:56:25 +0200

    implement upload file with supabase

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -6,12 +6,13 @@ import 'package:fruit_hub_dashboard/core/services/data_service.dart';
 import 'package:fruit_hub_dashboard/core/services/fire_storage.dart';
 import 'package:fruit_hub_dashboard/core/services/firestore_service.dart';
 import 'package:fruit_hub_dashboard/core/services/stoarage_service.dart';
+import 'package:fruit_hub_dashboard/core/services/supabase_stoarge.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 void setupGetit() {
-  getIt.registerSingleton<StoarageService>(FireStorage());
+  getIt.registerSingleton<StoarageService>(SupabaseStoargeService());
   getIt.registerSingleton<DatabaseService>(FireStoreService());
 
   getIt.registerSingleton<ImagesRepo>(
--- lib/core/services/supabase_stoarge.dart
+++ lib/core/services/supabase_stoarge.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:fruit_hub_dashboard/core/services/stoarage_service.dart';
 import 'package:fruit_hub_dashboard/core/utils/keys.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:path/path.dart' as b;
 
 class SupabaseStoargeService implements StoarageService {
   static late Supabase _supabase;
@@ -14,5 +15,13 @@ class SupabaseStoargeService implements StoarageService {
   }
 
   @override
-  Future<String> uploadFile(File file, String path) {}
+  Future<String> uploadFile(File file, String path) async {
+    String fileName = b.basename(file.path);
+    String extensionName = b.extension(file.path);
+    var resutl = await _supabase.client.storage
+        .from('fruits_images')
+        .upload('$path/$fileName.$extensionName', file);
+
+    return resutl;
+  }
 }
