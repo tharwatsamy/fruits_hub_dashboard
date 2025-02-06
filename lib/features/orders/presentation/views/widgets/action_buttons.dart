158c4b7255ecadbeef48d4a0ee14e9b3b735efe7
tharwat <tharwatsamy2004@gmail.com>
2025-02-06 15:14:30 +0200

    create actions buttons

---
+++ lib/features/orders/presentation/views/widgets/action_buttons.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import '../../../../../core/enums/order_enum.dart';
+import '../../../domain/entities/data/models/order_entity.dart';
+
+class ActionButtons extends StatelessWidget {
+  const ActionButtons({
+    super.key,
+    required this.orderModel,
+  });
+
+  final OrderEntity orderModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Visibility(
+          visible: orderModel.status == OrderEnum.pending,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text('Accept'),
+          ),
+        ),
+        Visibility(
+          visible: orderModel.status == OrderEnum.pending,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text('Reject'),
+          ),
+        ),
+        Visibility(
+          visible: orderModel.status == OrderEnum.accepted,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text('Delivered'),
+          ),
+        ),
+      ],
+    );
+  }
+}
